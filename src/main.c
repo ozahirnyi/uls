df60a96f45aea775c8907127fa09611cf4f1041e
ozahirnyi <ozahirny@gmail.com>
2020-02-20 17:56:28 +0200

    main is done, parsing work correctly

--- src/main.c
+++ src/main.c
@@ -1,38 +1,34 @@
 #include "uls.h"
 
-//static int way_counter(char **argv) {
-//    int i = 1;
-//    int counter = 0;
-//
-//    while (argv[i]) {
-//        if (argv[i][0] != '-')
-//            counter++;
-//        i++;
-//    }
-//    return counter;
-//}
-
-static void parser(char **argv, int argc, s_flags *flags, char **ways) {
-//    int ways_count = ways_counter(argv);
+static void parser(char **argv, int argc, s_flags *flags) {
+    char **ways = NULL;
     int index = 1;
+    int i = 0;
 
-    for (; argv[index][0] == '-' && argv[index][1] != '-'; index++)
-        for (int i = 0; argv[index][i]; i++)
-            mx_flags_trig(argv[index][i], flags);
+    for (; argv[index] && argv[index][0] == '-' && argv[index][1]; index++) {
+        if (argv[index][1] == '-' && !argv[index][2]) {
+            index++;
+            break;
+        }
+        for (int q = 1; argv[index][q]; q++)
+            mx_flags_trig(argv[index][q], flags);
+    }
     ways = (char **)malloc(sizeof(char *) * argc - index + 1);
-    for (int i = 0; index < argc; index++, i++)
+    for (; index < argc; index++, i++)
         ways[i] = mx_strdup(argv[index]);
+    ways[i] = NULL;
+    mx_read_uls(ways, flags);
+    mx_del_strarr(&ways);
 }
 
 int main(int argc, char **argv) {
-    char **ways = NULL;
     s_flags *flags = mx_flags_obnulyator();
 
     if (argc > 1)
-        parser(argv, argc, flags, ways);
-    mx_read_uls(ways, flags);
-    mx_del_strarr(&ways);
-    printf("\n\n");
-    system("leaks -q uls");
+        parser(argv, argc, flags);
+    else
+        mx_read_uls(NULL, flags);
+//    printf("\n\n");
+//    system("leaks -q uls");
     return 0;
 }
