26c9a16146aabb5e011226648903a7aa36530984
Nazar <53997458+NazarWild@users.noreply.github.com>
2020-02-28 16:59:50 +0200

    change ALL defines

--- src/mx_flag_p.c
+++ src/mx_flag_p.c
@@ -9,7 +9,7 @@ void mx_flag_p(t_list *files, s_flags *fl, char *argv) {
     while (p) {
         full_path = mx_strjoin_for_path(argv, p->data, fl);
         lstat(full_path, &buf);
-        if ((buf.st_mode & S_IFDIR) == S_IFDIR) {
+        if ((buf.st_mode & MX_IFDIR) == MX_IFDIR) {
         	buffer = mx_strdup(p->data);
         	free(p->data);
         	p->data = mx_strjoin(buffer, "/");
